96394047194828ca1560fddb677d07d79bcc5459
Cristian Buffa <bufface@gmail.com>
2016-12-24 16:51:27 -0300

    C 2.4

---
+++ 02/02constantes/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	const nombre = "Cristian"
+
+	// constantes no pormiten reasignación
+	// nombre = "Emanuel" -> No se permite
+
+	fmt.Println(nombre)
+}
