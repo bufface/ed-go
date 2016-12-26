aa107ca449cb33a6315386cd099cba3def0636e5
Cristian Buffa <bufface@gmail.com>
2016-12-26 12:47:55 -0300

    C 2.6

--- 02/02constantes/main.go
+++ 02/02constantes/main.go
@@ -10,15 +10,44 @@ Por convención, solo se utiliza para comentar código
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	const nombre = "Cristian"
+	// const nombre = "Cristian"
 
 	/*
 		  constantes no pormiten reasignación
 			nombre = "Emanuel" -> No se permite
 	*/
 
-	fmt.Println(nombre)
+	// fmt.Println(nombre)
+
+	// Tipos de datos (casteo)
+	/* var a int
+	var b int8
+	n := "Cristian"
+	p := "Buffa"
+
+	a = 121212
+	b = 5
+
+	c := a + int(b)
+
+	fmt.Println(c)
+	fmt.Printf("Hola %s %s %d , ¿Cómo estás?\n", n, p, b)
+	fmt.Printf("La variable b es de tipo: %T", b)
+
+	// prioridad artimética
+	// () % * / + -
+	// d := 6 + 6*(6-6)
+	// fmt.Println(d)
+	*/
+
+	// Valor 0 de las variables
+	var nombre string  // valor 0 = ""
+	var numero int     // valor 0 = 0
+	var entiendes bool // valor 0 = false
+	fmt.Println(nombre, numero, entiendes)
 }
