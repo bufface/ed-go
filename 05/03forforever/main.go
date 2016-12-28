65605517ce22dc8442d9f6d4469106cd5f95f998
Cristian Buffa <bufface@gmail.com>
2016-12-28 16:44:52 -0300

    C 5.3

---
+++ 05/03forforever/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Es infinito
+	// uno de sus usos más comunes es para esuchar en "sockets"
+	// tembien para escuchar conexiones, etc
+	for {
+		fmt.Println("Hola")
+	}
+}
