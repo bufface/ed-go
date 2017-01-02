59c4811ad1c721621c58ba538a50ac502d380e41
Cristian Buffa <bufface@gmail.com>
2017-01-02 20:46:43 -0300

    C 6.3 (faltaba 'push' de un archivo)

--- 06/01funciones/main.go
+++ 06/01funciones/main.go
@@ -4,6 +4,12 @@ import "fmt"
 
 func saludar(nombre string, edad uint8) {
 	fmt.Printf("Hola %s, tienes %d años de edad\n", nombre, edad)
+
+	if edad > 30 {
+		return
+	}
+
+	fmt.Println("Eres Joven")
 }
 
 func main() {
